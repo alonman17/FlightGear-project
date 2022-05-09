079114cb9ae73d279cee2cb8492cdd788e9f25bd
danielohayon420 <danielohayon420@gmail.com>
2022-05-09 14:00:42 +0300

    Initial Project

---
+++ front/src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+    <h1> Flight Gear Project</h1>
+    <div>
+      <ul>
+          <li>Daniel Ohayon </li>
+          <li>Alon Manshari </li>
+          <li>Rom Bardugo </li>
+          <li>Shon Hazan </li>
+          <li>Jony Singer </li>
+      </ul>
+      </div>
+    </header>
+    </div>
+  );
+}
+
+export default App;
