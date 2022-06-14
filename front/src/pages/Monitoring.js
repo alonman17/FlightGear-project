a0d0bc851a5d30f87b664d208f7beccf2b7d6244
Jonysinger1 <jonysinger1@gmail.com>
2022-06-14 11:44:07 +0300

    start monitoring

--- front/src/pages/Monitoring.js
+++ front/src/pages/Monitoring.js
@@ -1,7 +1,16 @@
 import Sidebar from "../components/nav-bar/nav_bar"
+import VirtualizedList from "../components/listOfFeatures"
+import { BarChart } from "../components/barChart"
+import { Joystick } from "react-joystick-component"
 export default function Monitoring (){
     return (<div >
-    <Sidebar />
-   <h1 style={{position:'absolute',left:'40%' , top:'30%'}}> Monitoring</h1>
-</div>)
+         <Sidebar />
+        <h1 style={{position:'absolute',left:'40%' , top:'10%'}}> Monitoring</h1>
+        <div style={{position:'absolute' , left:'25%',top:'25%' }}>
+            <VirtualizedList /> 
+        </div>
+        <div style={{position:'absolute' , right:'10%',top:'25%' ,width:'40vw'}}>
+            <Joystick />
+        </div>
+    </div>)
 }
