5ec385cc55942f70730f627623e328864feff90b
shonhazan123 <75479393+shonhazan123@users.noreply.github.com>
2022-06-18 13:06:25 +0300

    minor bug

--- Agent/main/src/main/java/tirgul/Controller/Controller.java
+++ Agent/main/src/main/java/tirgul/Controller/Controller.java
@@ -9,8 +9,8 @@ public class Controller {
 
         controller = new AgentController();
         System.out.println("Trying to connect to Backend ...");
-        controller.connectToDB("127.0.0.1", 5500);
+        controller.connectToBackendServer("127.0.0.1", 5500);
         System.out.println("Connected to DB");
-        new Thread(() -> controller.streamDataFromFG(AgentServer.client)).start();
+        new Thread(() -> controller.streamDataFromFG(AgentServer.clientSocket)).start();
     }
 }
